127c41183aa021a84fd7278db45cf329114bba2e
NedimRexhepi2 <nmr@inscale.net>
2026-07-30 13:19:55 +0200

    added backend regex

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,field_validator
+import re
 from enum import Enum
 from fastapi import Depends, HTTPException, status, Request
 from schemas import User
@@ -63,6 +64,16 @@ class UserModelCreate(BaseModel):
     username: str
     password: str
 
+@field_validator("password")
+@classmethod
+def validate_password(cls, v: str) -> str:
+    if len(v) < 8:
+      raise ValueError("Password must be at least 8 characters long.")
+    if not re.search(r"[A-Z]", v):
+      raise ValueError("Password must contain at least one uppercase letter.")
+    if not re.search(r"[0-9]", v):
+      raise ValueError("Password must contain at least one number.")
+    return v
 
 class UserModelEdit(BaseModel):
     username: str
